8055f9310cfe50668a9298fc20925fc4cf904ccd
selfxz <sebassalinasobregon@gmail.com>
2025-04-19 22:50:25 -0500

    server

--- config/database.js
+++ config/database.js
@@ -7,8 +7,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || 'mysql',
-    logging: false,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT || 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
   }
 );
 
